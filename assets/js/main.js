c2bb8978414ff6a8f18299a55df34af2e0f4463d
contconcord <contconcord6@gmail.com>
2022-12-17 14:23:25 +0500

    My template

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var options = {
+    animateClass: 'animate_animated', // for v3 or 'animate__animated' for v4
+    animateThreshold: 100,
+    scrollPollInterval: 20
+}
+$('.home-section').AniView();
